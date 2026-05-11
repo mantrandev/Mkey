95af8a52f8fd4fd02c7abe1d3cf993d18d407edc
mantrandev <mantrandev@gmail.com>
2026-05-12 01:36:55 +0700

    refactor: strip OpenKeyManager to initEventTap/stopEventTap/isInited only

--- Sources/OpenKey/macOS/ModernKey/OpenKeyManager.h
+++ Sources/OpenKey/macOS/ModernKey/OpenKeyManager.h
@@ -11,21 +11,10 @@
 
 #import <Cocoa/Cocoa.h>
 
-typedef void (^CheckNewVersionCallback)(void);
-
 @interface OpenKeyManager : NSObject
 +(BOOL)isInited;
 +(BOOL)initEventTap;
 +(BOOL)stopEventTap;
-
-+(NSArray*)getTableCodes;
-
-+(NSString*)getBuildDate;
-+(void)showMessage:(NSWindow*)window message:(NSString*)msg subMsg:(NSString*)subMsg;
-
-+(BOOL)quickConvert;
-
-+(void)checkNewVersion:(NSWindow*)parent callbackFunc:(CheckNewVersionCallback) callback;
 @end
 
 #endif /* OpenKeyManager_h */
